b0843b4ece7a105ec9df4fe567ca4209b3eb52f2
Charbie <eve.charbonneau.1@umontreal.ca>
2025-09-23 17:01:43 +0200

    EOD: state_estimstimator beginning

---
+++ ReachingLearning/StochasticOptimalControl/state_estimator/geometrical_state_estimate.py
@@ -0,0 +1,43 @@
+import casadi as cas
+
+
+def get_states_from_muscle_lengths(arm_model: "ArmModel", muscle_lengths):
+    """
+    Given mono-articular muscle lengths, it compute the corresponding joint angles (q) by averaging the estimates from
+    the agonist/antagonist pair.
+        - Shoulder angle from posterior deltoid and anterior deltoid
+        - Elbow angle from brachialis and lateral triceps
+    """
+    # Shoulder
+
+    arm_model.dM_coefficients = np.array(
+        [
+            [0,      0,       0.0100, 0.0300,  -0.0110, 1.9000],
+            [0,      0,       0.0100, -0.0190, 0,       0.0100],
+            [0.0400, -0.0080, 1.9000, 0,       0,       0.0100],
+            [-0.0420, 0,      0.0100, 0,       0,       0.0100],
+            [0.0300, -0.0110, 1.9000, 0.0320,  -0.0100, 1.9000],
+            [-0.0390, 0,      0.0100, -0.0220, 0,       0.0100],
+        ]
+    )
+
+    arm_model.a_shoulder = arm_model.dM_coefficients[:, 0]
+    arm_model.b_shoulder = arm_model.dM_coefficients[:, 1]
+    arm_model.c_shoulder = arm_model.dM_coefficients[:, 2]
+    arm_model.a_elbow = arm_model.dM_coefficients[:, 3]
+    arm_model.b_elbow = arm_model.dM_coefficients[:, 4]
+    arm_model.c_elbow = arm_model.dM_coefficients[:, 5]
+
+    l_full = (
+            arm_model.a_shoulder * theta_shoulder
+            + arm_model.b_shoulder * cas.sin(arm_model.c_shoulder * theta_shoulder) / arm_model.c_shoulder
+            + arm_model.a_elbow * theta_elbow
+            + arm_model.b_elbow * cas.sin(arm_model.c_elbow * theta_elbow) / arm_model.c_elbow
+    )
+
+    elbow = (
+            + arm_model.a_elbow * theta_elbow
+            + arm_model.b_elbow * cas.sin(arm_model.c_elbow * theta_elbow) / arm_model.c_elbow
+    )
+
+    return cas.vertcat(q1, q2)
---
+++ tests/test_state_estimator.py
@@ -0,0 +1,30 @@
+import numpy as np
+import numpy.testing as npt
+
+from ReachingLearning import (ArmModel, get_states_from_muscle_lengths)
+
+
+def test_get_states_from_muscle_lengths():
+
+    elbow_angles = [15*np.pi/180, 30*np.pi/180, 45*np.pi/180, 60*np.pi/180]
+    shoulder_angles = [15*np.pi/180, 30*np.pi/180, 45*np.pi/180, 60*np.pi/180]
+
+    arm_model = ArmModel(sensory_noise_magnitude = 0,
+                         motor_noise_magnitude = 0,
+                         force_field_magnitude = 0)
+
+    for shoulder in shoulder_angles:
+        for elbow in elbow_angles:
+
+            # Generate the data
+            q = np.array([shoulder, elbow])
+            muscle_lengths = arm_model.get_muscle_length(q)
+
+            # Test the functions and its inverse
+            q_computed = get_states_from_muscle_lengths(arm_model, muscle_lengths)
+            npt.assert_almost_equal(q, q_computed, decimal=5)
+
+            # Test that the noised version ios not too bad
+            noise = np.random.normal(0, 0.002, muscle_lengths.shape)
+            q_estimated = get_states_from_muscle_lengths(arm_model, muscle_lengths + noise)
+            npt.assert_almost_equal(q, q_estimated, decimal=2)
