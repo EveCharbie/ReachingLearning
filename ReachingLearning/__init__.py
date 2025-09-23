8599e53761e3cf9cbf77193891d612654d172442
Charbie <eve.charbonneau.1@umontreal.ca>
2025-09-23 17:00:40 +0200

    model

---
+++ ReachingLearning/__init__.py
@@ -0,0 +1,6 @@
+# models
+from .StochasticOptimalControl.models.arm_model import ArmModel
+
+# state_estimator
+from .StochasticOptimalControl.state_estimator.geometrical_state_estimate import get_states_from_muscle_lengths
+
