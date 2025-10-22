6cd46430af0ec46b8aaa09ec1b37b9fec5d058c3
Charbie <eve.charbonneau.1@umontreal.ca>
2025-10-22 18:00:31 +0200

    Auto stash before revert of "0.00001 easy, 0.0001 not possible !"

--- codes_to_run/main_run_socp.py
+++ codes_to_run/main_run_socp.py
@@ -33,8 +33,7 @@ final_time = 0.8
 n_shooting = int(final_time / dt)
 tol = 1e-6
 
-# motor_noise_std = 0.05  # taus
-motor_noise_std = 0.00001  # muscle_activations
+motor_noise_std = 0.05  # taus
 wPq_std = 3e-4
 wPqdot_std = 0.0024
 
